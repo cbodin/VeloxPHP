012dddba30d03ee9688a8fc36854c9ab1fdc82c9
Christofer Bodin <christofer@christoferbodin.se>
2014-02-23 17:18:47 +0100

    Fixed indention in jquery fallback

--- framework/js/jquery.fallback.js
+++ framework/js/jquery.fallback.js
@@ -1,6 +1,6 @@
 // Fallback to local copy of jquery
 if (!window.jQuery) {
-    document.write('<script src="' + Velox.Settings.jqueryPath +'"><\/script>');
+  document.write('<script src="' + Velox.Settings.jqueryPath +'"><\/script>');
 }
 
 // To avoid conflict with other libraries, remove the global $
