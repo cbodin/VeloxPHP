76ca1157935530933609825999c930d0f0d81d27
Christofer Bodin <christofer@christoferbodin.se>
2014-03-17 21:41:22 +0100

    VeloxFacebook: Cleaned up js events.

--- framework/modules/VeloxFacebook/velox.facebook.js
+++ framework/modules/VeloxFacebook/velox.facebook.js
@@ -1,19 +1,35 @@
 ;(function () {
+
   Velox.Facebook = {};
 
   Velox.Facebook.isReady = false;
-  Velox.Facebook.readyCallbacks = [];
 
-  Velox.Facebook.ready = function (callback) {
-    if (this.isReady) {
-      callback.apply(this);
+  Velox.Facebook.events = {
+    ready: []
+  };
+
+  Velox.Facebook.on = function(event, callback) {
+    if (event == 'ready') {
+      if (this.isReady) {
+        callback.apply(this);
+        return;
+      }
     }
-    else {
-      this.readyCallbacks.push(callback);
+
+    this.events[event].push(callback);
+  };
+
+  Velox.Facebook.trigger = function(event, data) {
+    if (event == 'ready') {
+      this.isReady = true;
+    }
+
+    for (var i=0, c=this.events[event].length; i<c; i++) {
+      this.events[event][i].call(this, data);
     }
   };
 
-  Velox.Facebook.inCanvas = function () {
+  Velox.Facebook.inCanvas = function() {
     return (window.location.search.indexOf('fb_sig_in_iframe=1') > -1) ||
            (window.location.search.indexOf('session=') > -1) ||
            (window.location.search.indexOf('signed_request=') > -1) ||
@@ -21,15 +37,15 @@
            (window.name.indexOf('app_runner') > -1);
   };
 
-  Velox.Facebook.getAppId = function () {
+  Velox.Facebook.getAppId = function() {
     return Velox.Settings.VeloxFacebook.appId;
   };
 
-  Velox.Facebook.getLocale = function () {
+  Velox.Facebook.getLocale = function() {
     return Velox.Settings.VeloxFacebook.locale;
   };
 
-  Velox.Facebook.getChannelUrl = function () {
+  Velox.Facebook.getChannelUrl = function() {
     return Velox.Settings.VeloxFacebook.channelUrl;
   };
 
@@ -43,14 +59,7 @@
       cookie     : true
     });
 
-    Velox.Facebook.isReady = true;
-    var readyCallbacks = Velox.Facebook.readyCallbacks;
-    
-    if (readyCallbacks.length) {
-      for (var i=0, c=readyCallbacks.length; i<c; i++) {
-        readyCallbacks[i].apply(Velox.Facebook);
-      }
-    }
+    Velox.Facebook.trigger('ready');
   };
 
   // Load the SDK asynchronously
@@ -61,4 +70,5 @@
     js.src = '//connect.facebook.net/' + Velox.Facebook.getLocale() + '/all.js';
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
+
 })();
