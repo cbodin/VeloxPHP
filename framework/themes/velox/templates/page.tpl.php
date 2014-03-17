885219aeef8d99923811de06d27443f0d2ee935b
Christofer Bodin <christofer@christoferbodin.se>
2014-03-17 13:02:41 +0100

    Updated velox page template

--- framework/themes/velox/templates/page.tpl.php
+++ framework/themes/velox/templates/page.tpl.php
@@ -1,10 +1,6 @@
-<div id="wrapper">
-	<div id="page-header">
-		<?php if ($page_title !== false): ?>
-			<h1><?php print $page_title; ?></h1>
-		<?php endif; ?>
-	</div>
-	<div id="content">
-		<?php print $content; ?>
-	</div>
+<div id="content">
+  <div class="messages">
+    <?php print Message::getString(); ?>
+  </div>
+  <?php print $content; ?>
 </div>
